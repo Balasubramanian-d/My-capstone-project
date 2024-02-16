df87f71adcf3720094dc2fdbd8fb6b0f8efff6fb
Balasubramanian-d <143893846+Balasubramanian-d@users.noreply.github.com>
2024-02-16 15:11:55 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 docker login -u balasubramaniand -p dckr_pat_bLlyFbCSTx8iEbJFOrcRNRS8dWw
-
+if [[ $GIT_BRANCH == "dev" ]]; then
+docker tag capstone balasubramaniand/dev
+docker push balasubramaniand/dev
+elif [[ $GIT_BRANCH == "main" ]]; then
 docker tag capstone balasubramaniand/prod
 docker push balasubramaniand/prod
 docker-compose up -d
